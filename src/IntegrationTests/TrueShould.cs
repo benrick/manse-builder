f16e4fccd654f13a40ba165156a2aa3a5d9df82c
Brendan Enrick <brendan@enrick.com>
2017-05-21 13:07:19 -0400

    Initial Project Structure Commit

---
+++ src/IntegrationTests/TrueShould.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ManseBuilder.IntegrationTests
+{
+    public class TrueShould
+    {
+        [Fact]
+        public void BeTrue()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
---
+++ src/UnitTests/TrueShould.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ManseBuilder.UnitTests
+{
+    public class TrueShould
+    {
+        [Fact]
+        public void BeTrue()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
